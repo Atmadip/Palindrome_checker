de3f8b1b039ad7fd82ac917799030f0d1ef5a564
Atmadip <52615332+Atmadip@users.noreply.github.com>
2024-05-07 14:34:58 -0500

    Update palindrome_tb.v

--- palindrome_tb.v
+++ palindrome_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/25/2024 06:44:48 PM
-// Design Name: 
-// Module Name: palindrome_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module palindrome_tb;
 parameter Data_width = 8;
